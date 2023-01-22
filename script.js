ffd4876c678d7425dd68e0967e2534bf456d7687
Carla C <skapkun@gmail.com>
2023-01-21 23:45:04 -0400

    Implementando datas e função save

--- script.js
+++ script.js
@@ -1,11 +1,33 @@
+ 
 const form = document.querySelector("form")
 const nlwSetup = new NLWSetup(form)
+const button = document.querySelector('header button')
+button.addEventListener('click', add)
+form.addEventListener ("change", save)
+function add () {
+  const today = new Date().toLocaleDateString("pt-br").slice(0, -5)
+   
+  const dayExists = nlwSetup.dayExists(today)
+  if (dayExists) {
+    alert ("Dia já incluso! ⛔")
+    return 
+  } 
+  alert ("Adicionado com sucesso! ✔")
+  nlwSetup.addDay (today)
+}
 
-const data = {
+function save (){
+  localStorage.setItem("NLWSetup@habits", JSON.stringify(nlwSetup.data))
+}
+
+const data = JSON.parse (localStorage.getItem ("NLWSetup@habits"))
+
+
+/*const data = {
   gym: ["01-01", "01-02", "01-06", "01-07", "01-08"],
   takePills: ["01-03"],
   study: ["01-02"],
 }
-
+*/
 nlwSetup.setData(data)
 nlwSetup.load()

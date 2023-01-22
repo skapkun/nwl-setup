be8f847df1939d76388cbf5a1c3472eb9240a89b
Carla C <skapkun@gmail.com>
2023-01-22 00:34:43 -0400

    Modificaçãozinha em mensagem do alert em caso de dia já incluso

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function add () {
    
   const dayExists = nlwSetup.dayExists(today)
   if (dayExists) {
-    alert ("Dia já incluso! ⛔")
+    alert ("Dia já incluso!")
     return 
   } 
   alert ("Adicionado com sucesso! ✔")
